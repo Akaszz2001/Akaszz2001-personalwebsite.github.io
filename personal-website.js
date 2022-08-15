f6a25b99f57e5ad4359501bdb0c98de745e7167a
akash krishna <akashkrishna.m@gpckasaragod.ac.in>
2022-08-15 23:31:26 +0530

    javascript file addedd, section part almost completed

---
+++ personal-website.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  $(".card ").mouseenter(function () { 
+   $(this).css({"background-color":"#28a745"})
+  });
+  $(".card ").mouseleave(function () { 
+   $(this).css({"background-color":"white"})
+  });
+
+   
+})
+
+
+
